fb438e28e3601606a168721088ab141b57df44d2
lily1215z <odessa1215@i.ua>
2023-03-01 21:25:02 +0100

    fix: delete .map in Nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,51 +1,66 @@
 import React from 'react';
 import nav from './Nav.module.scss'
-import home from '../home/Home.module.scss';
-import {NavSvgSelector} from '../iconSVG/NavSVGSelector';
+import home from "../home/Home.module.scss";
+import {NavSvgSelector} from "../iconSVG/NavSVGSelector";
 import {NavLink} from 'react-router-dom';
 
 export const Nav = () => {
+    // const navLinkStypes = ({isActive}): any => {
+    //     return {
+    //         borderColor: isActive ? 'red' : 'blue',
+    //         border: isActive ? '1px' : '2px'
+    //     }
+    // }
+    // const location = useLocation()
+
     const color = {
         color: '#ff651c'
     }
 
-    const navigationData = [
-        {
-            title: 'Home',
-            icon: 'house',
-        },
-        {
-            title: 'About',
-            icon: 'about',
-        },
-        {
-            title: 'Skills',
-            icon: 'skills',
-        },
-        {
-            title: 'Portfolio',
-            icon: 'portfolio',
-        },
-        {
-            title: 'Contacts',
-            icon: 'contacts',
-        },
-    ]
     return (
         <nav className={nav.inner}>
             <ul className={nav.list}>
-
-                {navigationData.map(item => {
-                    return <li className={nav.item}>
-                        <span className={nav.text_hover}>{item.title}</span>
-                        <NavLink to="/" style={({isActive}) => ({stroke: isActive ? color.color : ''})}>
-                            <div className={home.svg}>
-                                <NavSvgSelector icon={item.icon}/>
-                            </div>
-                        </NavLink>
-                    </li>
-                })}
-
+                <li className={nav.item}>
+                    <span className={nav.text_hover}>Home</span>
+                    <NavLink to='/' style={({isActive}) => ({stroke: isActive ? color.color : ''})}>
+                        {/*<NavLink to='/' className={({ isActive }) =>isActive ? " active-link" : ""}>*/}
+                        <div className={home.svg}>
+                            <NavSvgSelector icon={'house'}/>
+                        </div>
+                    </NavLink>
+                </li>
+                <li className={nav.item}>
+                    <span className={nav.text_hover}>About</span>
+                    <NavLink to='/about' style={({isActive}) => ({stroke: isActive ? color.color : ''})}>
+                        <div className={home.svg}>
+                            <NavSvgSelector icon={'about'}/>
+                        </div>
+                    </NavLink>
+                </li>
+                <li className={nav.item}>
+                    <span className={nav.text_hover}>Skills</span>
+                    <NavLink to='/skills' style={({isActive}) => ({stroke: isActive ? color.color : ''})}>
+                        <div className={home.svg}>
+                            <NavSvgSelector icon={'skills'}/>
+                        </div>
+                    </NavLink>
+                </li>
+                <li className={nav.item}>
+                    <span className={nav.text_hover}>Portfolio</span>
+                    <NavLink to='/portfolio' style={({isActive}) => ({stroke: isActive ? color.color : ''})}>
+                        <div className={home.svg}>
+                            <NavSvgSelector icon={'portfolio'}/>
+                        </div>
+                    </NavLink>
+                </li>
+                <li className={nav.item}>
+                    <span className={nav.text_hover}>Contacts</span>
+                    <NavLink to='/contacts' style={({isActive}) => ({stroke: isActive ? color.color : ''})}>
+                        <div className={home.svg}>
+                            <NavSvgSelector icon={'contacts'}/>
+                        </div>
+                    </NavLink>
+                </li>
             </ul>
 
         </nav>
