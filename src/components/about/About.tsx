7e72aa2d1478c8b39671182082d515997cce88aa
lily1215z <odessa1215@i.ua>
2023-03-11 13:23:05 +0100

    fix: text

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -12,20 +12,6 @@ import {Resume} from './Resume';
 
 export const About = () => {
 
-    // const onButtonClick = () => {
-    //     fetch('cv.pdf').then(response => {
-    //         response.blob().then(blob => {
-    //             // Creating new object of PDF file
-    //             const fileURL = window.URL.createObjectURL(blob);
-    //             // Setting various property values
-    //             let alink = document.createElement('a');
-    //             alink.href = fileURL;
-    //             alink.download = 'cv.pdf';
-    //             alink.click();
-    //         })
-    //     })
-    // }
-
     return (
         <>
             <div className={home.wrapper}>
@@ -54,7 +40,8 @@ export const About = () => {
                             tools as React, TypeScript, Redux, JS, HTML5 & CSS3, JavaScript, AXIOS, Git, Thunk, Rest
                             API,
                             Redux ToolKit. Ready for new challenges. I am Ukrainian, from the wonderful city of Odessa.
-                            But at the moment I live in Ciudad Real, Spain. I have a work permit. Open to your suggestions.
+                            But at the moment I live in Ciudad Real, Spain and I plan to move to Barcelona. I have a work permit.
+                            Open to your suggestions.
                         </p>
 
                         <div>
@@ -63,12 +50,12 @@ export const About = () => {
                             <div className={about.box}>
                                 <ul className={about.list}>
                                     <li className={home.text}>First Name : <span
-                                        className={about.style_text_white}>Mysliatska</span></li>
-                                    <li className={home.text}>Last Name : <span
                                         className={about.style_text_white}>Svitlana</span></li>
-                                    <li className={home.text}>From : <span className={about.style_text_white}>Ciudad Real, Spain</span>
+                                    <li className={home.text}>Last Name : <span
+                                        className={about.style_text_white}>Mysliatska</span></li>
+                                    <li className={home.text}>Located at : <span className={about.style_text_white}>Ciudad Real, Spain</span>
                                     </li>
-                                    <li className={home.text}>Relocate in : <span className={about.style_text_white}>Barcelona, Spain</span>
+                                    <li className={home.text}>Relocate to : <span className={about.style_text_white}>Barcelona, Spain</span>
                                     </li>
                                 </ul>
                                 <ul className={about.list}>
@@ -84,9 +71,7 @@ export const About = () => {
                             <div className={about.social}>
                                 {<SocialSvgComponent/>}
                             </div>
-                            {/*<button onClick={onButtonClick}>Download my CV*/}
-                            {/*    /!*<a href={cv} download="lana_cv">Download my CV</a>*!/*/}
-                            {/*</button>*/}
+
                             <div className={about.download}>
                                 <a className={about.download_link} href={cv} target={'_blank'} download="lana_cv">Download my CV</a>
                             </div>
